19b488594b8d48dc02de3480a78a1a34dea227ee
jarqprog <jarqprog@gmail.com>
2018-04-16 21:47:48 +0200

    add resources

--- src/main/java/com/jarq/App.java
+++ src/main/java/com/jarq/App.java
@@ -1,10 +1,20 @@
 package com.jarq;
 
 
+import com.jarq.server.MyServer;
+
+import java.io.IOException;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Start");
-    }
 
+        final int PORT = 8080;
+
+        try {
+            MyServer.create(PORT).run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
