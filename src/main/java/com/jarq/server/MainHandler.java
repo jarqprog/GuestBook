c8cc9e087f37de6f1be480aa9ec72976e65336ec
jarqprog <jarqprog@gmail.com>
2018-04-22 20:56:43 +0200

    implement all methods in main handler

--- src/main/java/com/jarq/server/MainHandler.java
+++ src/main/java/com/jarq/server/MainHandler.java
@@ -7,8 +7,13 @@ import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainHandler implements HttpHandler {
 
@@ -32,22 +37,68 @@ public class MainHandler implements HttpHandler {
         System.out.println("uri: " + uri);
 
         if (method.equals("GET")) {
-            renderPage(httpExchange);
+            switch (uri) {
+                case "/":
+                    renderPage(httpExchange);
+                    break;
+                case "/form":
+                    renderForm(httpExchange);
+                    break;
+            }
         }
 
         if (method.equals("POST")) {
-
-            renderPage(httpExchange);
-            captureData(httpExchange);
+            switch (uri) {
+                case "/form":
+                    captureData(httpExchange);
+                    break;
+            }
 
         }
 
     }
 
+    private void renderForm(HttpExchange httpExchange) throws IOException {
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("/static/form.html");
+
+        // create a model that will be passed to a template
+        JtwigModel model = JtwigModel.newModel();
+
+        model.with("result", "");
+
+        // render a template to a string
+        String response = template.render(model);
+
+        sendResponse(httpExchange, response);
+    }
+
     private void captureData(HttpExchange httpExchange) throws IOException {
 
-        System.out.println("jestem w capture data");
+        Map<String,String> inputs = getInput(httpExchange);
+        String guestName = inputs.get("guestName");
+        String messageContent = inputs.get("messageContent");
 
+        boolean isExportSuccess =  messageController.saveMessage(messageContent, guestName);
+
+        String result;
+        if(! isExportSuccess) {
+            result = "problem occurred while adding Your message to repository, we will check it..";
+        } else {
+            result = "Your message has been successfully added to repository!";
+        }
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("/static/form.html");
+
+        // create a model that will be passed to a template
+        JtwigModel model = JtwigModel.newModel();
+
+        model.with("result", result);
+
+        // render a template to a string
+        String response = template.render(model);
+
+        sendResponse(httpExchange, response);
     }
 
     private void renderPage(HttpExchange httpExchange) throws IOException {
@@ -74,6 +125,22 @@ public class MainHandler implements HttpHandler {
         os.write(response.getBytes());
         os.close();
 
+    }
+
+    private Map<String,String> getInput(HttpExchange httpExchange) throws IOException {
+
+        InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "utf-8");
+        BufferedReader br = new BufferedReader(isr);
+        String data = br.readLine();
+        Map<String,String> map = new HashMap<>();
+        System.out.println("parser: " + data);
+        String[] pairs = data.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            String value = URLDecoder.decode(keyValue[1], "UTF-8");
+            map.put(keyValue[0], value);
+        }
+        return map;
     }
 
 }
