9227b02df75856b0ef869abc2841255c4f78ae80
jarqprog <jarqprog@gmail.com>
2018-04-24 19:02:48 +0200

    implement changes to run App from JAR

--- src/main/java/com/jarq/server/StaticHandler.java
+++ src/main/java/com/jarq/server/StaticHandler.java
@@ -21,71 +21,65 @@ public class StaticHandler implements HttpHandler {
         // get file path from url
         URI uri = httpExchange.getRequestURI();
 
-        // get file path from url
-        String path = uri.getPath().substring(1);  // remove '/' for executable jar file purposes
-
-        System.out.println("looking for: " + path);
+        // get file path from url, remove '/' for executable jar file purposes
+        String path = uri.getPath().substring(1);
 
         ClassLoader classLoader = getClass().getClassLoader();
+
         InputStream inputStream = classLoader.getResourceAsStream(path);
         URL fileURL = classLoader.getResource(path);
 
-        if (inputStream == null) {
-
-            System.out.println("Not found static");
-
+        if (inputStream == null || fileURL == null) {
             // Object does not exist or is not a file: reject with 404 error.
             send404(httpExchange);
         } else {
-
-            System.out.println("Found it! static");
-
-
             // Object exists and is a file: accept with response code 200.
-            sendFile(httpExchange, fileURL, inputStream);
+            sendFile(httpExchange, inputStream, fileURL);
         }
     }
 
     private void send404(HttpExchange httpExchange) throws IOException {
-        String response = "404 (Not Found)\n";
-        httpExchange.sendResponseHeaders(404, response.length());
+
+        String PATH_TO_404_IMAGE = FilePaths.INFO404.getPath();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        InputStream inputStream = classLoader.getResourceAsStream(PATH_TO_404_IMAGE);
+        URL fileURL = classLoader.getResource(PATH_TO_404_IMAGE);
+        if(fileURL != null) {
+            sendFile(httpExchange, inputStream, fileURL);
+        } else {
+            sendSimple404(httpExchange);
+        }
+    }
+
+    private void sendFile(HttpExchange httpExchange, InputStream inputStream, URL fileURL) throws IOException {
+
+        File file = new File(fileURL.getFile());
+
+        // we need to find out the mime type of the file, see: https://en.wikipedia.org/wiki/Media_type
+        MimeTypeResolver resolver = new MimeTypeResolver(file);
+        String mime = resolver.getMimeType();
+
+        httpExchange.getResponseHeaders().set("Content-Type", mime);
+        httpExchange.sendResponseHeaders(200, 0);
+
         OutputStream os = httpExchange.getResponseBody();
-        os.write(response.toString().getBytes());
+
+        // send the file
+        final byte[] buffer = new byte[0x10000];
+        int count;
+        while ((count = inputStream.read(buffer)) >= 0) {
+            os.write(buffer, 0, count);
+        }
         os.close();
     }
 
-    private void sendFile(HttpExchange httpExchange, URL fileURL, InputStream inputStream) throws IOException {
-        // get the file
-        File file = new File(fileURL.getFile());
-
-
-        try {
-            // we need to find out the mime type of the file, see: https://en.wikipedia.org/wiki/Media_type
-            MimeTypeResolver resolver = new MimeTypeResolver(file);
-            String mime = resolver.getMimeType();
-
-
-
-            httpExchange.getResponseHeaders().set("Content-Type", mime);
-            httpExchange.sendResponseHeaders(200, 0);
-
-            OutputStream os = httpExchange.getResponseBody();
-
-            System.out.println("jestem przy OS!");
-            // send the file
-            System.out.println("jestem przy FIS!");
-            final byte[] buffer = new byte[0x10000];
-            int count;
-            while ((count = inputStream.read(buffer)) >= 0) {
-                os.write(buffer, 0, count);
-            }
-
-            System.out.println("os: " + os.toString());
-            os.close();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new IOException();
-        }
-
+    private void sendSimple404(HttpExchange httpExchange) throws IOException {
+        String response = "404 (Not Found)\n";
+        httpExchange.sendResponseHeaders(404, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
